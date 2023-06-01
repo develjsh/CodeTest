9350da0bbcbe64053cd79904b4a195b1026eb9cb
Jung Seung Hwan <develjsh@gmail.com>
2023-06-01 17:32:03 +0900

    Edit time sec to show ss.ms

--- TimeCalculator.py
+++ TimeCalculator.py
@@ -2,8 +2,8 @@ import time
 import datetime
 
 start = time.time()
+
 end = time.time()
 sec = (end - start)
-
-result_list = str(datetime.timedelta(seconds=sec)).split(".")
-print(result_list[0])
+sec = datetime.timedelta(seconds=sec)
+print(sec)
